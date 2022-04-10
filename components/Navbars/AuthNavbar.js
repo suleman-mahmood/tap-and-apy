328239458107de8c30cd62b1fd35cc952192a293
Suleman Mahmood <sulemanmahmood99@gmail.com>
2022-04-11 01:05:50 +0500

    fixed ui of login and register

--- components/Navbars/AuthNavbar.js
+++ components/Navbars/AuthNavbar.js
@@ -5,102 +5,68 @@ import Link from "next/link";
 import PagesDropdown from "components/Dropdowns/PagesDropdown.js";
 
 export default function Navbar(props) {
-  const [navbarOpen, setNavbarOpen] = React.useState(false);
-  return (
-    <>
-      <nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <Link href="/">
-              <a
-                className="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
-                href="#pablo"
-              >
-                Tap and Pay
-              </a>
-            </Link>
-            <button
-              className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-              type="button"
-              onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-              <i className="text-white fas fa-bars"></i>
-            </button>
-          </div>
-          <div
-            className={
-              "lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none" +
-              (navbarOpen ? " block rounded shadow-lg" : " hidden")
-            }
-            id="example-navbar-warning"
-          >
-            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-              <Link href="/" className="flex items-center mr-4">
-                <a
-                  className="hover:text-white text-white px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                >
-                  <span className="ml-2">About Us</span>
-                </a>
-              </Link>
-              <Link href="/auth/login" className="flex items-center">
-                <a
-                  className="hover:text-white text-white px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                >
-                  <span className="ml-2">Sign In</span>
-                </a>
-              </Link>
-              <Link href="/auth/register" className="flex items-center">
-                <a
-                  className="hover:text-white text-white px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                >
-                  <span className="ml-2">Sign Up</span>
-                </a>
-              </Link>
-              <li className="flex items-center">
-                <a
-                  className="hover:text-white text-white px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                  href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdemos.creative-tim.com%2Fnotus-nextjs%2F"
-                  target="_blank"
-                >
-                  <i className="text-blueGray-400 fab fa-facebook text-lg leading-lg " />
-                  <span className="lg:hidden inline-block ml-2">Share</span>
-                </a>
-              </li>
+	const [navbarOpen, setNavbarOpen] = React.useState(false);
+	return (
+		<>
+			<nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg">
+				<div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+					<div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+						<Link href="/">
+							<a className="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase" href="#pablo">
+								Tap and Pay
+							</a>
+						</Link>
+						<button
+							className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+							type="button"
+							onClick={() => setNavbarOpen(!navbarOpen)}
+						>
+							<i className="text-white fas fa-bars"></i>
+						</button>
+					</div>
+					<div className={"lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none" + (navbarOpen ? " block rounded shadow-lg" : " hidden")} id="example-navbar-warning">
+						<ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+							<Link href="/" className="flex items-center mr-4">
+								<a className="hover:text-white text-white px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold">
+									<span className="ml-2">About Us</span>
+								</a>
+							</Link>
+							<Link href="/auth/login" className="flex items-center">
+								<a className="hover:text-white text-white px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold">
+									<span className="ml-2">Sign In</span>
+								</a>
+							</Link>
+							<Link href="/auth/register" className="flex items-center">
+								<a className="hover:text-white text-white px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold">
+									<span className="ml-2">Sign Up</span>
+								</a>
+							</Link>
 
-              <li className="flex items-center">
-                <a
-                  className="hover:text-white text-white px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                  href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fdemos.creative-tim.com%2Fnotus-nextjs%2F&text=Start%20your%20development%20with%20a%20Free%20Tailwind%20CSS%20and%20NextJS%20UI%20Kit%20and%20Admin.%20Let%20Notus%20NextJS%20amaze%20you%20with%20its%20cool%20features%20and%20build%20tools%20and%20get%20your%20project%20to%20a%20whole%20new%20level."
-                  target="_blank"
-                >
-                  <i className="text-blueGray-400 fab fa-twitter text-lg leading-lg " />
-                  <span className="lg:hidden inline-block ml-2">Tweet</span>
-                </a>
-              </li>
+							<li className="flex items-center">
+								<a
+									className="hover:text-white text-white px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+									href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fdemos.creative-tim.com%2Fnotus-nextjs%2F&text=Start%20your%20development%20with%20a%20Free%20Tailwind%20CSS%20and%20NextJS%20UI%20Kit%20and%20Admin.%20Let%20Notus%20NextJS%20amaze%20you%20with%20its%20cool%20features%20and%20build%20tools%20and%20get%20your%20project%20to%20a%20whole%20new%20level."
+									target="_blank"
+								>
+									<i className="text-blueGray-400 fab fa-instagram text-lg leading-lg " />
+									<span className="lg:hidden inline-block ml-2">Instagram</span>
+								</a>
+							</li>
 
-              <li className="flex items-center">
-                <a
-                  className="hover:text-white text-white px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                  href="https://github.com/creativetimofficial/notus-nextjs?ref=nnjs-index-navbar"
-                  target="_blank"
-                >
-                  <i className="text-blueGray-400 fab fa-github text-lg leading-lg " />
-                  <span className="lg:hidden inline-block ml-2">Star</span>
-                </a>
-              </li>
-
-              <li className="flex items-center">
-                <button
-                  className="bg-blueGray-700 text-white active:bg-blueGray-600 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
-                  type="button"
-                >
-                  <i className="fas fa-arrow-alt-circle-down"></i> Download
-                </button>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+							<li className="flex items-center">
+								<a
+									className="hover:text-white text-white px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+									href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdemos.creative-tim.com%2Fnotus-nextjs%2F"
+									target="_blank"
+								>
+									<i className="text-blueGray-400 fab fa-facebook text-lg leading-lg " />
+									<span className="lg:hidden inline-block ml-2">Share</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</>
+	);
 }
--- pages/auth/register.js
+++ pages/auth/register.js
@@ -1,165 +1,168 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 // Firebase Authentication
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import {db} from "firebase-config";
-import {doc, setDoc } from "firebase/firestore"; 
+import { db } from "firebase-config";
+import { doc, setDoc } from "firebase/firestore";
+import Swal from "sweetalert2";
 
 // layout for page
 import Auth from "layouts/Auth.js";
 
 export default function Register() {
+	const router = useRouter();
 
-  const router = useRouter()
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [fullName, setFullName] = useState("");
 
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("");
-  const [fullName, setFullName] = useState("");
+	const authenticateEmail = () => {
+		if (email.includes("@lums.edu.pk")) return true;
+		return false;
+	};
 
-  const authenticateEmail = () => {
-    if(email.includes("@lums.edu.pk")) return true;
-    return false;
-  }
+	const register = () => {
+		if (!authenticateEmail()) {
+			console.log("Enter a LUMS email");
+			return;
+		}
 
-  const register = () => {
+		const auth = getAuth();
 
-    if(!authenticateEmail()){
-      console.log("Enter a LUMS email");
-      return;
-    }
+		createUserWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				// Signed in successful
+				const user = userCredential.user;
 
-    const auth = getAuth();
-    
-    createUserWithEmailAndPassword(auth, email, password)
-    .then(userCredential => {
-      // Signed in successful
-      const user = userCredential.user;
+				// Add data to firestore
+				const userDataRef = doc(db, "users", user.uid);
+				const docData = {
+					email: user.email,
+					fullName: fullName,
+					type: "customer",
+					balance: 0,
+				};
+				setDoc(userDataRef, docData)
+					.then(() => {
+						console.log("Added data entry successfully");
 
-      // Add data to firestore
-      const userDataRef = doc(db, "users", user.uid);
-      const docData = {
-        email: user.email,
-        fullName: fullName,
-        type: "customer",
-        balance: 0,
-      }
-      setDoc(userDataRef, docData)
-      .then(() => {
-        console.log("Added data entry successfully");
+						// Redirect user to dashboard
+						router.push("/user/dashboard");
+					})
+					.catch((error) => {
+						const errorMessage = error.message;
+						console.log("Error when setting up the document", errorMessage);
+					});
+			})
+			.catch((error) => {
+				const errorMessage = error.message;
+				console.log("Error when creating user with email and password", errorMessage);
+			});
+	};
 
-        // Redirect user to dashboard
-        router.push("/user/dashboard")
-      })
-      .catch((error) => {
-        const errorMessage = error.message;
-        console.log("Error when setting up the document", errorMessage);
-      })
-    })
-    .catch((error) => {
-      const errorMessage = error.message;
-      console.log("Error when creating user with email and password", errorMessage);
-    });
-  }
+	const showTermsAndConditions = () => {
+		Swal.fire("Any fool can use a computer");
+	};
 
-  return (
-    <>
-      <div className="container mx-auto px-4 h-full">
-        <div className="flex content-center items-center justify-center h-full">
-          <div className="w-full lg:w-6/12 px-4">
-            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
-              <div className="rounded-t mb-0 px-6 py-6">
-                <div className="text-center mb-3">
-                  <h6 className="text-blueGray-500 text-sm font-bold">
-                    Sign up with LUMS email id
-                  </h6>
-                </div>
-                <hr className="mt-6 border-b-1 border-blueGray-300" />
-              </div>
-              <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                <form>
-                  <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
-                    >
-                      Name
-                    </label>
-                    <input
-                      type="email"
-                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      placeholder="Name"
-                      onChange={e => setFullName(e.target.value)}
-                    />
-                  </div>
+	return (
+		<>
+			<div className="container mx-auto px-4 h-full">
+				<div className="flex content-center items-center justify-center h-full">
+					<div className="w-full lg:w-6/12 px-4">
+						<div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
+							<div className="rounded-t mb-0 px-6 py-6">
+								<div className="text-center mb-3">
+									<h6 className="text-blueGray-500 text-xl font-bold">Sign up with LUMS Email ID</h6>
+								</div>
+								<hr className="mt-6 border-b-1 border-blueGray-300" />
+							</div>
+							<div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+								<form>
+									<div className="relative w-full mb-3">
+										<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+											Name
+										</label>
+										<input
+											type="email"
+											className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+											placeholder="Name"
+											onChange={(e) => setFullName(e.target.value)}
+										/>
+									</div>
 
-                  <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
-                    >
-                      Email
-                    </label>
-                    <input
-                      type="email"
-                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      placeholder="Email"
-                      onChange={e => setEmail(e.target.value)}
-                    />
-                  </div>
+									<div className="relative w-full mb-3">
+										<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+											Email
+										</label>
+										<input
+											type="email"
+											className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+											placeholder="Email"
+											onChange={(e) => setEmail(e.target.value)}
+										/>
+									</div>
 
-                  <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
-                    >
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      placeholder="Password"
-                      onChange={e => setPassword(e.target.value)}
-                    />
-                  </div>
+									<div className="relative w-full mb-3">
+										<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+											Password
+										</label>
+										<input
+											type="password"
+											className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+											placeholder="Password"
+											onChange={(e) => setPassword(e.target.value)}
+										/>
+									</div>
 
-                  <div>
-                    <label className="inline-flex items-center cursor-pointer">
-                      <input
-                        id="customCheckLogin"
-                        type="checkbox"
-                        className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
-                      />
-                      <span className="ml-2 text-sm font-semibold text-blueGray-600">
-                        I agree with the{" "}
-                        <a
-                          href="#pablo"
-                          className="text-lightBlue-500"
-                          onClick={(e) => e.preventDefault()}
-                        >
-                          Privacy Policy
-                        </a>
-                      </span>
-                    </label>
-                  </div>
+									<div>
+										<label className="inline-flex items-center cursor-pointer">
+											<input id="customCheckLogin" type="checkbox" className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150" />
+											<span className="ml-2 text-sm font-semibold text-blueGray-600">
+												I agree with the{" "}
+												<a className="text-lightBlue-500" onClick={showTermsAndConditions}>
+													Terms and Conditions
+												</a>
+											</span>
+										</label>
+									</div>
 
-                  <div className="text-center mt-6">
-                    <button
-                      className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                      type="button"
-                      onClick={register}
-                    >
-                      Create Account
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+									<div className="text-center mt-6">
+										<button
+											className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 w-full ease-linear transition-all duration-150"
+											type="button"
+											onClick={register}
+										>
+											Create Account
+										</button>
+									</div>
+								</form>
+							</div>
+							<hr className="border-b-1 border-blueGray-300" />
+							<div className="rounded-t mb-0 px-6 py-6">
+								<div className="text-center mb-3">
+									<h6 className="text-blueGray-500 text-sm font-bold">Already have an account?</h6>
+								</div>
+								<div className="text-center mt-6">
+									<Link href="/auth/login">
+										<div className="text-center mt-6">
+											<button
+												className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 w-full ease-linear transition-all duration-150"
+												type="button"
+											>
+												Sign In
+											</button>
+										</div>
+									</Link>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 Register.layout = Auth;
