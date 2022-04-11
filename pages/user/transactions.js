e4701e7a799567dc1f5a1e56e855f4a6ee73a8f1
Suleman Mahmood <sulemanmahmood99@gmail.com>
2022-04-11 17:54:55 +0500

    created fetching transactions page

---
+++ pages/user/transactions.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+
+import { db } from "firebase-config";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+import User from "layouts/User.js";
+
+export default function Landing() {
+	const [userUid, setUserUid] = useState(-1);
+	const [transactionsList, setTransactionsList] = useState([]);
+
+	useEffect(() => {
+		const auth = getAuth();
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				// User is signed in
+				const uid = user.uid;
+				setUserUid(uid);
+
+				const docRef = collection(db, "transactions");
+				const q = query(docRef, where("sender", "==", uid));
+
+				onSnapshot(q, (querySnapshot) => {
+					if (querySnapshot.empty) {
+						console.log("No transactions found!");
+					}
+
+					querySnapshot.forEach((eachQueryDoc) => {
+						const oldList = transactionsList;
+						oldList.push(eachQueryDoc.data());
+						setTransactionsList(oldList);
+					});
+				});
+			} else {
+				// User is signed out
+				router.push("/");
+			}
+		});
+	}, []);
+
+	return (
+		<>
+			<main>
+				{/* Steps to deposit */}
+				<div className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-25">
+					<div
+						className="absolute top-0 w-full h-full bg-center bg-cover"
+						style={{
+							backgroundImage: "url('https://images.unsplash.com/photo-1556742502-ec7c0e9f34b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80')",
+						}}
+					>
+						<span id="blackOverlay" className="w-full h-full absolute opacity-75 bg-black"></span>
+					</div>
+					<div className="container relative mx-auto">
+						<div className="items-center flex flex-wrap">
+							<div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+								<div className="pr-12">
+									<h1 className="text-white font-semibold text-5xl">Transactions</h1>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-16" style={{ transform: "translateZ(0)" }}>
+						<svg className="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
+							<polygon className="text-blueGray-200 fill-current" points="2560 0 2560 100 0 100"></polygon>
+						</svg>
+					</div>
+				</div>
+
+				<section className="pb-20 bg-blueGray-200 -mt-24">
+					<div className="container mx-auto px-4">
+						<div className="flex flex-wrap">
+							<div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
+								<div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+									<div className="px-4 py-5 flex-auto">
+										<div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
+											<i className="fas fa-arrow-up"></i>
+										</div>
+										<h6 className="text-xl font-semibold">First, bank transfer the deposit amount to the following details</h6>
+										<p className="mt-2 mb-4 text-blueGray-500">
+											Account Title: <b>Suleman Mahmood</b>
+											<br />
+											Account Number: <b>01300105628521</b>
+											<br />
+											IBAN: <b>PK15MEZN0001300105628521</b>
+											<br />
+											Meezan Bank-Shamsi Society Branch - Malir <br />
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+			</main>
+		</>
+	);
+}
+
+Landing.layout = User;
