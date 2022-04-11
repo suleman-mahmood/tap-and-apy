33f119bb70dd9d32db648dd777bb190681af442e
Suleman Mahmood <sulemanmahmood99@gmail.com>
2022-04-11 06:33:52 +0500

    added transactions for security and bug fixes in the transfer page

--- pages/user/transfer.js
+++ pages/user/transfer.js
@@ -1,176 +1,175 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { doc, getDoc, collection, query, where, getDocs, updateDoc } from "firebase/firestore";
+import { doc, getDoc, collection, query, where, getDocs, updateDoc, runTransaction } from "firebase/firestore";
 import { useRouter } from "next/router";
-import {db} from "firebase-config";
-import { QrReader } from 'react-qr-reader';
-import Swal from 'sweetalert2'
+import { db } from "firebase-config";
+import { QrReader } from "react-qr-reader";
+import Swal from "sweetalert2";
 
 // layout for page
 import User from "layouts/User.js";
 
 export default function Dashboard() {
+	const router = useRouter();
+	const [userData, setUserData] = useState({});
 
-  const router = useRouter()
-  const [userData, setUserData] = useState({});
+	const [rollNumber, setRollNumber] = useState("");
+	const [amount, setAmount] = useState(0);
+	const [myUid, setMyUid] = useState("");
 
-  const [rollNumber, setRollNumber] = useState("");
-  const [amount, setAmount] = useState(0);
-  const [myUid, setMyUid] = useState("");
+	const [qrResult, setQrResult] = useState(-1);
 
-  const [qrResult, setQrResult] = useState(-1);
+	useEffect(() => {
+		const auth = getAuth();
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				const uid = user.uid;
+				setMyUid(uid);
 
-  useEffect(() => {
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        const uid = user.uid;
-        setMyUid(uid);
+				const docRef = doc(db, "users", uid);
+				getDoc(docRef).then((docSnap) => {
+					if (docSnap.exists()) {
+						console.log("Document data:", docSnap.data());
+						setUserData(docSnap.data());
+					} else {
+						// doc.data() will be undefined in this case
+						console.log("No such document!");
+					}
+				});
+			} else {
+				// User is signed out
+				router.push("/");
+			}
+		});
+	}, []);
 
-        const docRef = doc(db, "users", uid);
-        getDoc(docRef)
-        .then(docSnap => {
-          if (docSnap.exists()) {
-            console.log("Document data:", docSnap.data());
-            setUserData(docSnap.data());
-          } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-          }
-        })
+	const handleTransfer = () => {
+		if (amount <= 0) {
+			console.log("Don't enter a negative value");
+			return;
+		}
 
-      } else {
-        // User is signed out
-        router.push("/")
-      }
-    });
-  }, []);
+		const docRef = collection(db, "users");
+		const emailQuery = rollNumber + "@lums.edu.pk";
+		const q = query(docRef, where("email", "==", emailQuery));
 
-  const handleTransfer = () => {
+		getDocs(q).then((querySnapshot) => {
+			querySnapshot.forEach((incomingDoc) => {
+				const docId = incomingDoc.id;
+				// const docData = incomingDoc.data();
 
-    if(amount <= 0) {
-      console.log("Don't enter a negative value");
-      return;
-    }
+				runTransaction(db, (transaction) => {
+					const senderDocRef = doc(db, "users", myUid);
+					const recipientDocRef = doc(db, "users", docId);
 
-    const docRef = collection(db, "users");
-    const emailQuery = rollNumber + "@lums.edu.pk";
-    const q = query(docRef, where("email", "==", emailQuery))
+					return transaction.get(senderDocRef).then((senderDoc) => {
+						return transaction.get(recipientDocRef).then((recipientDoc) => {
+							// Update my doc (sender)
+							transaction.update(senderDocRef, {
+								balance: senderDoc.data().balance - parseInt(amount),
+							});
+							// Update recipient's doc
+							transaction.update(recipientDocRef, {
+								balance: recipientDoc.data().balance + parseInt(amount),
+							});
+							console.log("All done successfully!");
 
-    getDocs(q)
-    .then(querySnapshot => {
-      querySnapshot.forEach(incomingDoc => {
-        const docId = incomingDoc.id;
-        const docData = incomingDoc.data();
+							setUserData({
+								...userData,
+								balance: userData.balance - amount,
+							});
+							setAmount(0);
+							setRollNumber(0);
 
-        // Increment recipients account
-        updateDoc(doc(db, "users", docId), {
-          balance: parseInt(docData.balance) + parseInt(amount)
-        })
-        .then(() => {
+							Swal.fire({
+								title: "Payment Successful",
+								text: "You have successfully transferred Rs." + amount + " to " + rollNumber,
+								icon: "success",
+								confirmButtonText: "Cool",
+							});
 
-          // Deduct my amount
-          updateDoc(doc(db, "users", myUid), {
-            balance: parseInt(userData.balance) - parseInt(amount)
-          })
-          .then(() => {
-            setUserData({
-              ...userData,
-              balance: userData.balance - amount
-            });
-            setAmount(0);
-            setRollNumber(0);
+							return transaction;
+						});
+					});
+				});
+			});
+		});
+	};
 
-            Swal.fire({
-              title: 'Payment Successful',
-              text: 'You have successfully transferred Rs.' + amount + " to " + rollNumber,
-              icon: 'success',
-              confirmButtonText: 'Cool'
-            });
-          })
-        })
-      })
-    })
-  }
+	return (
+		<>
+			<div className="p-8 flex items-center flex-col justify-center">
+				<h1 className="text-2xl font-bold">Transfer Money</h1>
+				<div className="relative w-full mb-3">
+					<p className="my-6">Available Balance: {userData.balance}</p>
+					<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+						Amount
+					</label>
+					<input
+						type="number"
+						className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+						placeholder="Number"
+						onChange={(e) => setAmount(e.target.value)}
+						value={amount}
+					/>
+				</div>
+				<h1 className="text-center">Now Either scan a QR code to add recipients address or add it manually</h1>
+			</div>
 
-  return (
-    <>
-      <div className="p-8 flex items-center flex-col justify-center">
-        <h1 className="text-2xl font-bold" >Transfer Money</h1>
-        <div className="relative w-full mb-3">
-          <p className="my-6">Available Balance: {userData.balance}</p>
-          <label
-            className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-            htmlFor="grid-password"
-          >
-            Amount
-          </label>
-          <input
-            type="number"
-            className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-            placeholder="Number"
-            onChange={e => setAmount(e.target.value)}
-            value={amount}
-          />
-        </div>
-        <h1 className="text-center" >Now Either scan a QR code to add recipients address or add it manually</h1>
-      </div>
+			{qrResult != -1 ? (
+				<></>
+			) : (
+				<div className="flex items-center flex-col justify-center">
+					<h1 className="font-semibold">Scan QR code:</h1>
 
-      {qrResult != -1 ? <></> : 
-        <div className="flex items-center flex-col justify-center">
-          <h1 className="font-semibold" >Scan QR code:</h1>
-          
-          <QrReader
-            onResult={(result, error) => {
-              if (!!result) {
-                setQrResult(result?.text);
-                setRollNumber(result?.text);
-              }
+					<QrReader
+						onResult={(result, error) => {
+							if (!!result) {
+								setQrResult(result?.text);
+								setRollNumber(result?.text);
+							}
 
-              if (!!error) {
-                console.info(error);
-              }
-            }}
-            className="w-full"
-            constraints={{
-              facingMode: 'environment'
-            }}
-          />
-        </div>
-      }
+							if (!!error) {
+								console.info(error);
+							}
+						}}
+						className="w-full"
+						constraints={{
+							facingMode: "environment",
+						}}
+					/>
+				</div>
+			)}
 
-      <div className="flex items-center flex-col justify-center">
-        <h1 className="my-6 font-semibold" >Or enter details Manually:</h1>
-        <form>
-          <div className="relative w-full mb-3">
-            <label
-              className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-              htmlFor="grid-password"
-            >
-              Receivers Roll Number / Vendor's Address
-            </label>
-            <input
-              type="text"
-              className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-              placeholder="RollNumber"
-              value={rollNumber}
-              onChange={e => setRollNumber(e.target.value)}
-            />
-          </div>
+			<div className="flex items-center flex-col justify-center">
+				<h1 className="my-6 font-semibold">Or enter details Manually:</h1>
+				<form>
+					<div className="relative w-full mb-3">
+						<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+							Receivers Roll Number / Vendor's Address
+						</label>
+						<input
+							type="text"
+							className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+							placeholder="RollNumber"
+							value={rollNumber}
+							onChange={(e) => setRollNumber(e.target.value)}
+						/>
+					</div>
 
-          <div className="text-center mt-6">
-            <button
-              className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-              type="button"
-              onClick={handleTransfer}
-            >
-              Transfer
-            </button>
-          </div>
-        </form>
-      </div>
-    </>
-  );
+					<div className="text-center mt-6">
+						<button
+							className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+							type="button"
+							onClick={handleTransfer}
+						>
+							Transfer
+						</button>
+					</div>
+				</form>
+			</div>
+		</>
+	);
 }
 
 Dashboard.layout = User;
